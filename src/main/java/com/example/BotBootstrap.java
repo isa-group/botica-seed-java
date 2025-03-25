cd4458fee12ce8a007d0e0a87deb8fab11688173
Alberto Mimbrero <mimbrero.alberto@gmail.com>
2025-03-25 22:09:48 +0100

    chore: bump botica-lib-java to 0.4.0

--- src/main/java/com/example/BotBootstrap.java
+++ src/main/java/com/example/BotBootstrap.java
@@ -3,8 +3,7 @@ package com.example;
 import es.us.isa.botica.bot.BotLauncher;
 
 public class BotBootstrap {
-
   public static void main(String[] args) {
-    BotLauncher.run(new Bot(), args);
+    BotLauncher.run(new Bot());
   }
 }
